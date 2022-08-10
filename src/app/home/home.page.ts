de665a63545f89eeffbd9ca497dd2d07ff2ba578
Samuel <104946564+sampcunha@users.noreply.github.com>
2022-08-10 19:25:11 -0300

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,13 +10,25 @@ export class HomePage {
   constructor() {}
 
   variavel_lista = [];
-  texto: string = "";
+  texto = "";
 
 adiciona() {
   if (!(this.texto == "")) {
     this.variavel_lista.push(this.texto);
     this.texto = "";
-  } 
+  }
+  
+  /*
+  if (this.texto == "") {
+
+  } else{
+  this.variavel_lista.push(this.texto);
+  this.texto = "";
+  }*/
+}
+
+remove(indice) { 
+  this.variavel_lista.splice(indice, 1)
 }
 
   //*ngFor = "let item of minhaLista" no item
