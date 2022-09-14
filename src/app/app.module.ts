ca6313efd16dbd641309db914f9d2fa476d7b12d
Samuel <104946564+sampcunha@users.noreply.github.com>
2022-09-14 16:47:02 -0300

    att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { IonicStorageModule } from '@ionic/storage-angular';
-import { Drivers } from '@ionic/storage'
+import { Drivers } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
@@ -24,4 +24,4 @@ import { Drivers } from '@ionic/storage'
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,7 @@ import { Storage } from '@ionic/storage-angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
   constructor(private storage:Storage) {
@@ -13,46 +14,45 @@ export class HomePage {
   }
 
   variavel_lista = [];
-  texto: String = "";
-  aux;
-  
-async adiciona() {
-  if (!(this.texto == "")) {
-    this.variavel_lista.push(this.texto);
-    this.variavel_lista.forEach(item => {
-      if(parseInt(item[0]) > this.aux) {
-        this.aux = parseInt(item[0]);
-      }
-    })
-    this.aux = this.aux + 1;
-    await this.storage.set(this.aux.toString(), this.texto);
-    this.atualizaLita();
-    this.texto = "";
+  texto: string = "";
+  valor: number = 0;
+  soma = 0;
+  aux = 0;
+
+ngOnInit(){
+    this.atualizaLista();
   }
-  
-  /*
-  if (this.texto == "") {
 
-  } else{
-  this.variavel_lista.push(this.texto);
-  this.texto = "";
-  }*/
+atualizaLista() {
+    this.variavel_lista = [];
+    this.storage.forEach((value, key, index) => {
+	console.log(index);
+      this.variavel_lista.push([key, value]);
+    })
+
+    this.somar();
+  }
+
+  async adiciona() {
+    if ( !(this.texto == "") && !(this.valor < 0)) {
+      this.variavel_lista.push(this.texto);
+      this.aux = this.variavel_lista.length - 1
+	    await this.storage.set(this.aux.toString(), [this.texto, this.valor]);
+	    this.atualizaLista();
+      this.valor = 0;
+      this.texto = "";
+    }
+  }
+
+  async remove(indice) {
+    await this.storage.remove(indice);
+    this.atualizaLista();
+  }
+
+  somar() {
+    this.soma = 0;
+    this.storage.forEach((value, key, index) => {
+      this.soma += value[1];
+    })
+  }
 }
-
-atualizaLita() {
-  this.variavel_lista = []
-  this.storage.forEach((value, key, index) => {
-    this.variavel_lista.push([key, value])
-  }) 
-}
-
-remove(indice) { 
-  this.variavel_lista.splice(indice, 1)
-}
-
-  //*ngFor = "let item of minhaLista" no item
-  //[(ngModel)]="texto" no input
-
-}
-
-//texte de alteracao!!
